25fb3fb6cc02f4136f9a44a57fd3f06f858868be
KushnirIllia <illiakushnira@gmail.com>
2023-01-12 00:19:36 +0200

    Add App, Main, Navbar. Reset styles

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Main from "./components/Main"
+import Navbar from "./components/Navbar"
+
+function App() {
+	return (
+    <>
+			<Navbar />
+			<Main />
+		</>
+	)
+}
+export default App
---
+++ src/components/Main.js
@@ -0,0 +1,9 @@
+
+function Main() {
+	return (
+		<>
+			<h1>Main Comp</h1>
+		</>
+	)
+}
+export default Main
